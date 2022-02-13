bb6fb4add47d55544028273449de92efe122b49d
h73 <heikoschmidt82@gmail.com>
2022-02-13 21:31:51 +0100

    add photo zoom

--- www/assets/main.js
+++ www/assets/main.js
@@ -2,24 +2,44 @@ window.addEventListener("DOMContentLoaded", function () {
     setArticleEvents();
 });
 
+let gallery = null;
+
 function setArticleEvents() {
-    document.querySelectorAll("main article").forEach(function (article) {
-        article.addEventListener("click", function () {
-            loadPhotos(this);
+    document.querySelectorAll("main article .photos").forEach(function (article) {
+        article.addEventListener("click", function (e) {
+            if (gallery != null) closePhotoGallery(gallery);
+            if (gallery !== this) {
+                gallery = this;
+                loadPhotos(this, e.target);
+            } else gallery = null;
         });
     });
 }
 
-function loadPhotos(article) {
-    const photos = article.querySelectorAll(".photo");
+function loadPhotos(div_photos, div_photo) {
+    const photos = div_photos.querySelectorAll(".photo");
     let cnt = 0;
     photos.forEach(function (photo) {
         photo.addEventListener("load", function () {
+            if (this === div_photo) this.style.zIndex = photos.length + 1;
+            else this.style.zIndex = photos.length - cnt;
             cnt++;
             if (cnt === photos.length) {
-                console.log("FotosPhotos loaded")
+                openPhotoGallery(div_photos);
             }
         });
         photo.src = photo.getAttribute("data-photo");
     });
 }
+
+function openPhotoGallery(div_photos) {
+    div_photos.classList.add('show');
+    div_photos.style.height = div_photos.offsetWidth + 'px';
+}
+
+function closePhotoGallery(div_photos) {
+    div_photos.classList.remove('show');
+    div_photos.style.height = null;
+}
+
+3
