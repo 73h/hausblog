3b392600181e63c7ba6871171fa5889acd8e9779
h73 <heikoschmidt82@gmail.com>
2022-02-27 20:46:11 +0100

    refactor cms functions

--- src/app/App.php
+++ src/app/App.php
@@ -30,7 +30,7 @@ class App
         });
     }
 
-    private function render(
+    protected function render(
         string $site,
         string $subtitle,
         array  $parameters = [],
@@ -85,26 +85,6 @@ class App
         ], header_image: $header_image, description: $article['title']);
     }
 
-    public function login(string $code = null)
-    {
-        $message = '';
-        if ($code !== null) {
-            Auth::logInWithCode($code);
-            if (!Auth::isLoggedIn()) $message = 'Das Code ist ungültig oder abgelaufen.';
-            else {
-                $_SESSION['auth']['pk_user'] = Auth::$pk_user;
-                header('Location: /cms/articles');
-                exit;
-            }
-        }
-        $this->render('login', 'Login.',
-            [
-                'user' => Auth::$user,
-                'message' => $message
-            ], header_image_height: CMS_HEADER_IMAGE_HEIGHT
-        );
-    }
-
     public function webhook(array $data)
     {
         if (array_key_exists('message', $data) && array_key_exists('from', $data['message'])) {
@@ -116,116 +96,6 @@ class App
                 $telegram->receiveCommand($data['message']['text']);
             }
         }
-    }
-
-    public function cms_article_delete(int $pk_article)
-    {
-        Articles::deleteArticlePhotos($pk_article);
-        Articles::deleteArticle($pk_article);
-        header('Location: /cms/articles');
-        exit;
-    }
-
-    public function cms_article(?int $pk_article)
-    {
-        $message = '';
-        if ($pk_article !== null) $article = Articles::getArticle($pk_article, Auth::isLoggedIn() ? 0 : 1);
-        else {
-            $article = [
-                'created' => now_cet()->format('Y-m-d\TH:i'),
-                'title' => '',
-                'content' => '',
-                'published' => 0,
-                'photos' => []
-            ];
-        }
-        if (isset($_POST) && count($_POST) > 0) {
-            $photos = [];
-            foreach (explode(';', $_POST['photos']) as $pk_photo) {
-                array_push($photos, ['pk_photo' => intval($pk_photo)]);
-            }
-            $article['created'] = $_POST['created'];
-            $article['title'] = $_POST['title'];
-            $article['content'] = $_POST['content'];
-            $article['published'] = isset($_POST['published']) ? 1 : 0;
-            $article['photos'] = $photos;
-            if ($article['title'] != '' && $article['content'] != '') {
-                if ($pk_article == null) {
-                    $pk_article = Articles::insertArticle(
-                        $article['title'],
-                        $article['content'],
-                        $article['created'],
-                        $article['published']
-                    );
-                    $position = 1;
-                    foreach ($article['photos'] as $photo) {
-                        Articles::insertArticlePhoto($pk_article, $photo['pk_photo'], $position);
-                        $position++;
-                    }
-                } else {
-                    Articles::updateArticle(
-                        $pk_article,
-                        $article['title'],
-                        $article['content'],
-                        $article['created'],
-                        $article['published']
-                    );
-                    Articles::deleteArticlePhotos($pk_article);
-                    $position = 1;
-                    foreach ($article['photos'] as $photo) {
-                        Articles::insertArticlePhoto($pk_article, $photo['pk_photo'], $position);
-                        $position++;
-                    }
-                }
-                header('Location: /cms/articles');
-                exit;
-            } else {
-                $message = 'Der Titel und Inhalt dürfen nicht leer sein.';
-            }
-        }
-        $photos = Photos::getPhotos();
-        $emoticons = Articles::getEmoticons();
-        $this->render('cms_article', 'Eintrag bearbeiten.', [
-            'article' => $article,
-            'photos' => $photos,
-            'message' => $message,
-            'emoticons' => $emoticons
-        ], header_image_height: CMS_HEADER_IMAGE_HEIGHT);
-    }
-
-    public function cms_photo_delete(int $pk_photo)
-    {
-        Photos::deletePhotoFromArticles($pk_photo);
-        Photos::deletePhoto($pk_photo);
-        header('Location: /cms/photos');
-        exit;
-    }
-
-    public function cms_photo(int $pk_photo)
-    {
-        $message = '';
-        $photo = Photos::getPhotoData($pk_photo);
-        if ($photo == null) {
-            http_response_code(404);
-            exit;
-        }
-        if (isset($_POST) && count($_POST) > 0) {
-            $photo['title'] = $_POST['title'];
-            if ($photo['title'] != '') {
-                Photos::setPhotoTitle(
-                    $pk_photo,
-                    $photo['title']
-                );
-                header('Location: /cms/photos');
-                exit;
-            } else {
-                $message = 'Der Titel darf nicht leer sein.';
-            }
-        }
-        $this->render('cms_photo', 'Foto bearbeiten.', [
-            'photo' => $photo,
-            'message' => $message
-        ], header_image_height: CMS_HEADER_IMAGE_HEIGHT);
     }
 
     public function photo(int $pk_photo, string $id, bool $thumbnail)
@@ -250,24 +120,6 @@ class App
                 default => 'image/jpeg',
             });
         echo $photo['photo'];
-    }
-
-    public function cms_articles()
-    {
-        $articles = Articles::getArticles(0, 1000000, 0);
-        $this->render('cms_articles', 'Redaktion.', [
-            'articles' => $articles,
-            'cmsnav' => 1
-        ], header_image_height: CMS_HEADER_IMAGE_HEIGHT);
-    }
-
-    public function cms_photos()
-    {
-        $photos = Photos::getPhotos();
-        $this->render('cms_photos', 'Redaktion.', [
-            'photos' => $photos,
-            'cmsnav' => 2
-        ], header_image_height: CMS_HEADER_IMAGE_HEIGHT);
     }
 
 }
--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,7 @@
 
 use src\app\App;
 use src\app\Auth;
+use src\app\Cms;
 use Steampixel\Route;
 
 function redirectWhenNotAuth()
@@ -22,68 +23,6 @@ Route::add('/articles/([0-9]+)', function (int $pk_article) {
     $app->article($pk_article);
 }, ['get']);
 
-Route::add('/login', function () {
-    $app = new App();
-    $app->login($_POST['code'] ?? null);
-}, ['get', 'post']);
-
-Route::add('/login/([0-9]{6})', function ($code) {
-    $app = new App();
-    $app->login($code);
-}, ['get']);
-
-Route::add('/cms', function () {
-    header('Location: /cms/articles');
-    exit;
-}, ['get']);
-
-Route::add('/cms/articles', function () {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_articles();
-}, ['get']);
-
-Route::add('/cms/articles/([0-9]+)', function (int $pk_article) {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_article($pk_article);
-}, ['get', 'post']);
-
-Route::add('/cms/articles/([0-9]+)/delete', function (int $pk_article) {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_article_delete($pk_article);
-}, ['get']);
-
-Route::add('/cms/articles/new', function () {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_article(null);
-}, ['get', 'post']);
-
-Route::add('/cms/photos', function () {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_photos();
-}, ['get']);
-
-Route::add('/cms/photos/([0-9]+)', function (int $pk_photo) {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_photo($pk_photo);
-}, ['get', 'post']);
-
-Route::add('/cms/photos/([0-9]+)/delete', function (int $pk_photo) {
-    redirectWhenNotAuth();
-    $app = new App();
-    $app->cms_photo_delete($pk_photo);
-}, ['get']);
-
-Route::add('/logout', function () {
-    session_destroy();
-    header('Location: ' . URL);
-    exit;
-}, ['get']);
 
 Route::add('/photos/([0-9]+)/(tn/|)([0-9a-z]+)\.[a-z]+', function (int $pk_image, string $type, string $id) {
     $thumbnail = $type == 'tn/';
@@ -96,5 +35,68 @@ Route::add('/webhook', function () {
     $app = new App();
     $app->webhook($data);
 }, 'post');
+
+Route::add('/login', function () {
+    $cms = new Cms();
+    $cms->login($_POST['code'] ?? null);
+}, ['get', 'post']);
+
+Route::add('/login/([0-9]{6})', function ($code) {
+    $app = new Cms();
+    $app->login($code);
+}, ['get']);
+
+Route::add('/logout', function () {
+    session_destroy();
+    header('Location: ' . URL);
+    exit;
+}, ['get']);
+
+Route::add('/cms', function () {
+    header('Location: /cms/articles');
+    exit;
+}, ['get']);
+
+Route::add('/cms/articles', function () {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_articles();
+}, ['get']);
+
+Route::add('/cms/articles/([0-9]+)', function (int $pk_article) {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_article($pk_article);
+}, ['get', 'post']);
+
+Route::add('/cms/articles/([0-9]+)/delete', function (int $pk_article) {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_article_delete($pk_article);
+}, ['get']);
+
+Route::add('/cms/articles/new', function () {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_article(null);
+}, ['get', 'post']);
+
+Route::add('/cms/photos', function () {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_photos();
+}, ['get']);
+
+Route::add('/cms/photos/([0-9]+)', function (int $pk_photo) {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_photo($pk_photo);
+}, ['get', 'post']);
+
+Route::add('/cms/photos/([0-9]+)/delete', function (int $pk_photo) {
+    redirectWhenNotAuth();
+    $app = new Cms();
+    $app->cms_photo_delete($pk_photo);
+}, ['get']);
 
 Route::run('/');
