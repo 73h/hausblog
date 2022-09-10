dd7587f248dd33346cf021fbb813f9070f0820d9
h73 <heikoschmidt82@gmail.com>
2022-09-10 17:58:18 +0200

    delete console output

--- src/app/Articles.php
+++ src/app/Articles.php
@@ -75,7 +75,6 @@ class Articles
         preg_match_all('/YT\((.+)\)/', $content, $videos, PREG_SET_ORDER);
         foreach ($videos as $video) {
             $content = str_replace($video[0], '<div class="embedded-videos"><iframe src="https://www.youtube.com/embed/' . $video[1] . '" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>', $content);
-            console($video);
         }
         return $content;
     }
